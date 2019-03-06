a1eec5931689e5d4f14beefad3bf37639963ed63
Daan Nekeman <daanneek@gmail.com>
2019-03-06 12:12:01 +0100

    REEEEEEEEE

--- OplossingOP2(W).py
+++ OplossingOP2(W).py
@@ -1,3 +1,8 @@
 print(22%2)
 sadiasjjdoiafjg/poakokdsa[plas]
 HALLO IK BEN WEOUTER ERN IK HEb DIT BESTAND AANGEPSAPST!!!)))))))))
+
+
+HALLO WOUTER
+
+IK WIL NIET DAT JIJ NOG AAN DIT BESTAND KOMT <<<<>>>>
